4058737213d415a0da43cf2bfe7416acfef710f2
Ayithapu Sai Rithvik <rithvikayithapu@gmail.com>
2020-05-12 18:05:37 +0530

    updated database link

--- manage.py
+++ manage.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
@@ -5,7 +6,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flaskblog.models import User, Post
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
